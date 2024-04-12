cb69048ca27fbab3c8023e7a9f977d02be97eaee
Devansh Khare <devanshkhare0@gmail.com>
2024-04-12 19:25:44 +0530

    integrated gemini ai

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import discord
 import os
+import google.generativeai as genai
 
 from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv("SECRET_KEY")
+genai.configure(api_key=os.getenv("GEMINI_KEY"))
+
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config)
+
+convo = model.start_chat(history=[
+    {
+        "role": "user",
+        "parts": ["hi"]
+    },
+    {
+        "role": "model",
+        "parts": ["Hi there! 👋 How can I help you today?"]
+    },
+])
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -12,8 +35,10 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        convo.send_message(message.content)
+
         channel = message.channel
-        await channel.send('Say hello!')
+        await channel.send(convo.last.text)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,5 +46,3 @@ intents.message_content = True
 client = MyClient(intents=intents)
 client.run(token)
 
-# # app id: 1228304487639158895
-# # public key: 17bd2421abbd19876ba758ec7fbc557fbb7fa67c3da6eb099129494e59c85513
