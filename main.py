5c5f704187fcfe1d86c4dff76ac6bdd8ffb74610
Devansh Khare <devanshkhare0@gmail.com>
2024-04-13 01:31:05 +0530

    completed discord bot

--- main.py
+++ main.py
@@ -9,14 +9,32 @@ token = os.getenv("SECRET_KEY")
 genai.configure(api_key=os.getenv("GEMINI_KEY"))
 
 generation_config = {
-  "temperature": 0.9,
+  "temperature": 1,
   "top_p": 1,
   "top_k": 1,
-  "max_output_tokens": 2048,
+  "max_output_tokens": 256,
 }
 
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
 
-model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config)
+model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config, safety_settings=safety_settings)
 
 convo = model.start_chat(history=[
     {
@@ -35,10 +53,12 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        convo.send_message(message.content)
+        if self.user!=message.author:
+            if self.user in message.mentions:
+                convo.send_message(message.content)
 
-        channel = message.channel
-        await channel.send(convo.last.text)
+                channel = message.channel
+                await channel.send(convo.last.text)
 
 intents = discord.Intents.default()
 intents.message_content = True
